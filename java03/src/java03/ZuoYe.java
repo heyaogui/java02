6082046833c70a980410bd48cbb1444cc97644bb
heyaogui <211920106@qq.com>
2018-04-27 09:21:22 +0800

    java

---
+++ java03/src/java03/ZuoYe.java
@@ -0,0 +1,16 @@
+package java03;
+
+public class ZuoYe {
+
+	public static void main(String[]args) {
+		int year;{
+		if(year%4==0&&year%100!=0||year%400==0)
+		
+			return true;
+		}else 
+			return false;
+		
+
+	}
+
+}
